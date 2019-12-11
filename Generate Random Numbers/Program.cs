c5ef6ce0a98561c3fa84019da064593d7017b76b
Sachin Gurung <saching061@hotmail.com>
2019-12-10 21:50:56 -0800

    First commit

--- Generate Random Numbers/Program.cs
+++ Generate Random Numbers/Program.cs
@@ -1,15 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Generate_Random_Numbers
+﻿/*
+ * C# Program to Generate Random Numbers
+ */
+using System;
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Console.WriteLine("Some Random Numbers that are generated are : ");
+        for (int i = 1; i < 10; i++)
         {
+            Randfunc();
         }
     }
+    static Random r = new Random();
+    static void Randfunc()
+    {
+        int n = r.Next();
+        Console.WriteLine(n);
+        Console.ReadLine();
+    }
 }
+/*
+Some Random Numbers that are generated are :
+423504359
+
+1114355466
+
+1478185975
+
+673460588
+
+1049725257
+
+711372861
+
+149717477
+
+954732525
+
+1918860834
+
+Press any key to continue . . .
+*/
